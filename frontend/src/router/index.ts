b4710c805ad0fff915ee4220d14d17fb097a6fe3
Po-Chang-Chen <t108590033@ntut.org.tw>
2024-03-23 16:28:55 +0800

    toyconfirm router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import VertifyToy from '@/views/VertifyToy.vue'
+import ToyConfirm from '@/views/Toyconfirm.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,10 @@ const router = createRouter({
       path: '/v',
       name: '/v',
       component: VertifyToy
+    }, {
+      path: '/confirm',
+      name: '/confirm',
+      component: ToyConfirm
     }
   ]
 })
---
+++ frontend/src/views/Toyconfirm.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="content-center">
+    <ToyConfirm/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ToyConfirm from '@/components/eth-toyconfirm.vue'
+
+</script>

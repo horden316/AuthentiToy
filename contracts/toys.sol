852f5bb84e9e4a7c6681aa78878295bc23480cf9
horden316 <horden316@gmail.com>
2024-03-23 19:27:32 +0800

    feat: Soulbound NFT & add Type

--- contracts/toys.sol
+++ contracts/toys.sol
@@ -12,19 +12,22 @@ contract Toys is ERC721, Ownable(msg.sender)  {
     Counters.Counter private _tokenIdCounter;
 
     struct ToyInfo {
+        string Type;
         string ownerName;
         string toyName;
     }
 
     mapping(uint256 => ToyInfo) private _toyInfos;
+    mapping (address => uint256) private ownerToID;
 
     constructor() ERC721("Toys", "TOY") {}
 
-    function mint(address to, string memory toyName) public onlyOwner {
+    function mint(string memory _type, address to, string memory toyName) public onlyOwner {
         _tokenIdCounter.increment();
         uint256 tokenId = _tokenIdCounter.current();
         _mint(to, tokenId);
-        _toyInfos[tokenId] = ToyInfo("", toyName);
+        ownerToID[to]=tokenId;
+        _toyInfos[tokenId] = ToyInfo(_type, "", toyName);
     }
 
     function setOwnerName(uint256 tokenId, string memory ownerName) public {
@@ -37,8 +40,20 @@ contract Toys is ERC721, Ownable(msg.sender)  {
         _toyInfos[tokenId].toyName = toyName;
     }
 
-    function getToyInfo(uint256 tokenId) public view returns (string memory ownerName, string memory toyName) {
+    function getToyInfo(uint256 tokenId) public view returns (string memory _type, string memory ownerName, string memory toyName) {
         require(ownerOf(tokenId) != address(0), "Token does not exist");
-        return (_toyInfos[tokenId].ownerName, _toyInfos[tokenId].toyName);
+        return (_toyInfos[tokenId].Type, _toyInfos[tokenId].ownerName, _toyInfos[tokenId].toyName);
     }
+
+    function getTokenID(address owner) public view returns (uint256) {
+       return (ownerToID[owner]);
+    }
+    
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721) returns (address){
+    address from = _ownerOf(tokenId);
+    if (from != address(0) && to != address(0)) {
+        revert("Soulbound: Transfer failed");
+    }
+    return super._update(to, tokenId, auth);
+}
 }

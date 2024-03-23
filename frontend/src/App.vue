42b727b13202de2d715cbb90845a491539ac6515
detaomega <cosmolynnyang@gmail.com>
2024-03-23 16:30:52 +0800

    feat: add homepage link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,3 @@
-<style>
-  /* 去除 router-link 的下划线 */
-  .el-menu-item a.router-link {
-    text-decoration: none;
-  }
-</style>
 <template>
   <header>
     <el-menu class="el-menu-demo" mode="horizontal">
@@ -11,15 +5,21 @@
         <router-link to="/" class="router-link">home</router-link>
       </el-menu-item>
       <el-menu-item>
-        <router-link to="/v" class="router-link">verify</router-link>
+        <router-link to="/vertify" class="router-link">verify</router-link>
       </el-menu-item>
-
     </el-menu>
   </header>
 
   <br />
   <RouterView />
 </template>
+
+<style>
+/* 去除 router-link 的下划线 */
+.el-menu-item a.router-link {
+  text-decoration: none;
+}
+</style>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'

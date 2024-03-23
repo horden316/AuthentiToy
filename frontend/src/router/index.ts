7aaaf73b190367ce70342037a7ec41060fb3c18c
Po-Chang-Chen <t108590033@ntut.org.tw>
2024-03-23 13:54:58 +0800

    add page link

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-
+import VertifyToy from '@/views/VertifyToy.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: '/',
       component: HomeView
+    }
+    ,{
+      path: '/v',
+      name: '/v',
+      component: VertifyToy
     }
   ]
 })
---
+++ frontend/src/views/VertifyToy.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="content-center">
+    <VertifyToy/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VertifyToy from '@/components/eth-vertify.vue'
+
+</script>
